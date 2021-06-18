fb829466e2ceaf2d179e142f986bb5b735e019e8
Ryan Sanna <ryansann@umich.edu>
2021-06-18 10:38:15 -0700

    roletemplate rule validation, disallow empty verbs to avoid issues in rbac controllers

--- pkg/resources/roletemplate/roletemplate.go
+++ pkg/resources/roletemplate/roletemplate.go
@@ -1,12 +1,14 @@
 package roletemplate
 
 import (
+	"net/http"
 	"time"
 
 	rancherv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	"github.com/rancher/webhook/pkg/auth"
 	"github.com/rancher/wrangler/pkg/webhook"
 	admissionv1 "k8s.io/api/admission/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/trace"
 )
@@ -33,6 +35,20 @@ func (r *roleTemplateValidator) Admit(response *webhook.Response, request *webho
 	rules, err := r.escalationChecker.RulesFromTemplate(rt)
 	if err != nil {
 		return err
+	}
+
+	// ensure all PolicyRules have at least one verb, otherwise RBAC controllers may encounter issues when creating Roles and ClusterRoles
+	for _, rule := range rules {
+		if len(rule.Verbs) == 0 {
+			response.Result = &metav1.Status{
+				Status:  "Failure",
+				Message: "RoleTemplate.Rules: PolicyRules must have at least one verb",
+				Reason:  metav1.StatusReasonBadRequest,
+				Code:    http.StatusBadRequest,
+			}
+			response.Allowed = false
+			return nil
+		}
 	}
 
 	return r.escalationChecker.ConfirmNoEscalation(response, request, rules, "")
