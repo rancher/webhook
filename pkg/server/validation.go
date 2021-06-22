3db71ded3f0622a44d7eb42226c0fd95adf9a27f
Ryan Sanna <ryansann@umich.edu>
2021-06-22 10:39:55 -0700

    add validation logic around globalroles

--- pkg/server/validation.go
+++ pkg/server/validation.go
@@ -8,6 +8,7 @@ import (
 	"github.com/rancher/webhook/pkg/clients"
 	"github.com/rancher/webhook/pkg/resources/cluster"
 	"github.com/rancher/webhook/pkg/resources/clusterroletemplatebinding"
+	"github.com/rancher/webhook/pkg/resources/globalrole"
 	"github.com/rancher/webhook/pkg/resources/globalrolebinding"
 	"github.com/rancher/webhook/pkg/resources/projectroletemplatebinding"
 	"github.com/rancher/webhook/pkg/resources/roletemplate"
@@ -15,15 +16,17 @@ import (
 )
 
 func Validation(clients *clients.Clients) (http.Handler, error) {
-	globalRoleBindings := globalrolebinding.NewValidator(clients.Management.GlobalRole().Cache(), clients.EscalationChecker)
-	prtbs := projectroletemplatebinding.NewValidator(clients.Management.RoleTemplate().Cache(), clients.EscalationChecker)
-	crtbs := clusterroletemplatebinding.NewValidator(clients.Management.RoleTemplate().Cache(), clients.EscalationChecker)
-	roleTemplates := roletemplate.NewValidator(clients.EscalationChecker)
 	clusters := cluster.NewValidator(clients.K8s.AuthorizationV1().SubjectAccessReviews())
+	roleTemplates := roletemplate.NewValidator(clients.EscalationChecker)
+	globalRoles := globalrole.NewValidator()
+	globalRoleBindings := globalrolebinding.NewValidator(clients.Management.GlobalRole().Cache(), clients.EscalationChecker)
+	crtbs := clusterroletemplatebinding.NewValidator(clients.Management.RoleTemplate().Cache(), clients.EscalationChecker)
+	prtbs := projectroletemplatebinding.NewValidator(clients.Management.RoleTemplate().Cache(), clients.EscalationChecker)
 
 	router := webhook.NewRouter()
 	router.Kind("Cluster").Group(management.GroupName).Type(&v3.Cluster{}).Handle(clusters)
 	router.Kind("RoleTemplate").Group(management.GroupName).Type(&v3.RoleTemplate{}).Handle(roleTemplates)
+	router.Kind("GlobalRole").Group(management.GroupName).Type(&v3.GlobalRole{}).Handle(globalRoles)
 	router.Kind("GlobalRoleBinding").Group(management.GroupName).Type(&v3.GlobalRoleBinding{}).Handle(globalRoleBindings)
 	router.Kind("ClusterRoleTemplateBinding").Group(management.GroupName).Type(&v3.ClusterRoleTemplateBinding{}).Handle(crtbs)
 	router.Kind("ProjectRoleTemplateBinding").Group(management.GroupName).Type(&v3.ProjectRoleTemplateBinding{}).Handle(prtbs)
