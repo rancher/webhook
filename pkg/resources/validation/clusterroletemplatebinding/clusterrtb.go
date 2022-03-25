2771dd7c6103c41fef10c0b10ff0b5f8c8a4a01a
Ryan Sanna <ryansann@umich.edu>
2022-03-25 13:36:36 -0700

    Update CRTB/PRTB validation handlers to disallow updates to referenced role template

--- pkg/resources/validation/clusterroletemplatebinding/clusterrtb.go
+++ pkg/resources/validation/clusterroletemplatebinding/clusterrtb.go
@@ -1,13 +1,17 @@
 package clusterroletemplatebinding
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/rancher/webhook/pkg/auth"
 	v3 "github.com/rancher/webhook/pkg/generated/controllers/management.cattle.io/v3"
 	objectsv3 "github.com/rancher/webhook/pkg/generated/objects/management.cattle.io/v3"
 	"github.com/rancher/wrangler/pkg/webhook"
+	admissionv1 "k8s.io/api/admission/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/trace"
 )
 
@@ -26,6 +30,23 @@ type clusterRoleTemplateBindingValidator struct {
 func (c *clusterRoleTemplateBindingValidator) Admit(response *webhook.Response, request *webhook.Request) error {
 	listTrace := trace.New("clusterRoleTemplateBindingValidator Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
 	defer listTrace.LogIfLong(2 * time.Second)
+
+	// disallow updates to the referenced role template
+	if request.Operation == admissionv1.Update {
+		oldCrtb, newCrtb, err := objectsv3.ClusterRoleTemplateBindingOldAndNewFromRequest(request)
+		if err != nil {
+			return err
+		}
+		if oldCrtb.RoleTemplateName != newCrtb.RoleTemplateName {
+			response.Result = &metav1.Status{
+				Status:  "Failure",
+				Message: fmt.Sprintf("cannot update referenced roleTemplate for clusterRoleTemplateBinding %s", oldCrtb.Name),
+				Reason:  metav1.StatusReasonBadRequest,
+				Code:    http.StatusBadRequest,
+			}
+			return nil
+		}
+	}
 
 	crtb, err := objectsv3.ClusterRoleTemplateBindingFromRequest(request)
 	if err != nil {
