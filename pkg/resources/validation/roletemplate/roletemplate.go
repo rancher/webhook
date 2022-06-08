aaccab8407f82899f57b9e7c6358c7feb21e980b
Kevin Joiner <kevinjoiner@users.noreply.github.com>
2022-06-08 12:49:24 -0400

    Applies RT escalate check.

--- pkg/resources/validation/roletemplate/roletemplate.go
+++ pkg/resources/validation/roletemplate/roletemplate.go
@@ -7,9 +7,17 @@ import (
 	"github.com/rancher/webhook/pkg/auth"
 	objectsv3 "github.com/rancher/webhook/pkg/generated/objects/management.cattle.io/v3"
 	"github.com/rancher/wrangler/pkg/webhook"
+	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/utils/trace"
 )
+
+var roleTemplateGVR = schema.GroupVersionResource{
+	Group:    "management.cattle.io",
+	Version:  "v3",
+	Resource: "roletemplates",
+}
 
 func NewValidator(escalationChecker *auth.EscalationChecker) webhook.Handler {
 	return &roleTemplateValidator{
@@ -25,26 +33,26 @@ func (r *roleTemplateValidator) Admit(response *webhook.Response, request *webho
 	listTrace := trace.New("roleTemplateValidator Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
 	defer listTrace.LogIfLong(2 * time.Second)
 
-	rt, err := objectsv3.RoleTemplateFromRequest(request)
+	roleTemplate, err := objectsv3.RoleTemplateFromRequest(request)
 	if err != nil {
 		return err
 	}
 
 	// object is in the process of being deleted, so admit it
 	// this admits update operations that happen to remove finalizers
-	if rt.DeletionTimestamp != nil {
+	if roleTemplate.DeletionTimestamp != nil {
 		response.Allowed = true
 		return nil
 	}
 
-	rules, err := r.escalationChecker.RulesFromTemplate(rt)
+	rules, err := r.escalationChecker.RulesFromTemplate(roleTemplate)
 	if err != nil {
 		return err
 	}
 
 	// ensure all PolicyRules have at least one verb, otherwise RBAC controllers may encounter issues when creating Roles and ClusterRoles
-	for _, rule := range rules {
-		if len(rule.Verbs) == 0 {
+	for i := range rules {
+		if len(rules[i].Verbs) == 0 {
 			response.Result = &metav1.Status{
 				Status:  "Failure",
 				Message: "RoleTemplate.Rules: PolicyRules must have at least one verb",
@@ -52,8 +60,19 @@ func (r *roleTemplateValidator) Admit(response *webhook.Response, request *webho
 				Code:    http.StatusBadRequest,
 			}
 			response.Allowed = false
+
 			return nil
 		}
+	}
+
+	allowed, err := r.escalationChecker.EscalationAuthorized(response, request, roleTemplateGVR)
+	if err != nil {
+		logrus.Warnf("Failed to check for the 'escalate' verb on RoleTemplates: %v", err)
+	}
+
+	if allowed {
+		response.Allowed = true
+		return nil
 	}
 
 	return r.escalationChecker.ConfirmNoEscalation(response, request, rules, "")
