KevinJoiner/rancher-webhook

Restructered webhook registration.
Webhook request routing is now handled by K8s.

pkg/resources/mutation/machineconfigs/machineconfigs.go
@@ -2,37 +2,61 @@ package machineconfigs
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/rancher/webhook/pkg/generated/objects/core/unstructured"
+	"github.com/rancher/webhook/pkg/admission"
+	v1 "github.com/rancher/webhook/pkg/generated/objects/core/v1"
 	"github.com/rancher/webhook/pkg/resources/mutation"
-	"github.com/rancher/wrangler/pkg/webhook"
+	admissionv1 "k8s.io/api/admission/v1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/utils/trace"
 )
 
-func NewMutator() webhook.Handler {
-	return &mutator{}
+var gvr = schema.GroupVersionResource{
+	Group:    "rke-machine-config.cattle.io",
+	Version:  "v1",
+	Resource: "*",
 }
 
-type mutator struct {
+// Mutator implements admission.MutatingAdmissionWebhook.
+type Mutator struct{}
+
+// GVR returns the GroupVersionKind for this CRD.
+func (m *Mutator) GVR() schema.GroupVersionResource {
+	return gvr
 }
 
-func (m *mutator) Admit(response *webhook.Response, request *webhook.Request) error {
+// Operations returns list of operations handled by this mutator.
+func (m *Mutator) Operations() []admissionregistrationv1.OperationType {
+	return []admissionregistrationv1.OperationType{admissionregistrationv1.Create}
+}
+
+// MutatingWebhook returns the MutatingWebhook used for this CRD.
+func (m *Mutator) MutatingWebhook(clientConfig admissionregistrationv1.WebhookClientConfig) *admissionregistrationv1.MutatingWebhook {
+	mutatingWebhook := admission.NewDefaultMutationWebhook(m, clientConfig, admissionregistrationv1.NamespacedScope)
+	mutatingWebhook.SideEffects = admission.Ptr(admissionregistrationv1.SideEffectClassNoneOnDryRun)
+	return mutatingWebhook
+}
+
+// Admit is the entrypoint for the mutator. Admit will return an error if it unable to process the request.
+func (m *Mutator) Admit(request *admission.Request) (*admissionv1.AdmissionResponse, error) {
 	if request.DryRun != nil && *request.DryRun {
-		response.Allowed = true
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Allowed: true,
+		}, nil
 	}
 
 	listTrace := trace.New("machine config Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
-	defer listTrace.LogIfLong(2 * time.Second)
+	defer listTrace.LogIfLong(admission.SlowTraceDuration)
 
-	config, err := unstructured.UnstructuredFromRequest(&request.AdmissionRequest)
+	config, err := v1.UnstructuredFromRequest(&request.AdmissionRequest)
 	if err != nil {
-		return fmt.Errorf("failed to get object from request: %w", err)
+		return nil, fmt.Errorf("failed to get object from request: %w", err)
 	}
+	response := &admissionv1.AdmissionResponse{}
 	if err := mutation.SetCreatorIDAnnotation(request, response, request.Object, config.DeepCopy()); err != nil {
-		return fmt.Errorf("failed to set creatorIDAnnotation: %w", err)
+		return nil, fmt.Errorf("failed to set creatorIDAnnotation: %w", err)
 	}
 	response.Allowed = true
-	return nil
+	return response, nil
 }

pkg/resources/mutation/secret/secret.go
@@ -2,41 +2,64 @@ package secret
 
 import (
 	"fmt"
-	"time"
 
+	"github.com/rancher/webhook/pkg/admission"
 	"github.com/rancher/webhook/pkg/auth"
+	objectsv1 "github.com/rancher/webhook/pkg/generated/objects/core/v1"
 	"github.com/rancher/webhook/pkg/patch"
-	"github.com/rancher/wrangler/pkg/webhook"
 	"github.com/sirupsen/logrus"
 	admissionv1 "k8s.io/api/admission/v1"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/utils/trace"
 )
 
-func NewMutator() webhook.Handler {
-	return &mutator{}
+var gvr = schema.GroupVersionResource{
+	Group:    "",
+	Version:  "v1",
+	Resource: "secrets",
 }
 
-type mutator struct{}
+// Mutator implements admission.MutatingAdmissionWebhook.
+type Mutator struct{}
 
-func (m *mutator) Admit(response *webhook.Response, request *webhook.Request) error {
+// GVR returns the GroupVersionKind for this CRD.
+func (m *Mutator) GVR() schema.GroupVersionResource {
+	return gvr
+}
+
+// Operations returns list of operations handled by this mutator.
+func (m *Mutator) Operations() []admissionregistrationv1.OperationType {
+	return []admissionregistrationv1.OperationType{admissionregistrationv1.Create}
+}
+
+// MutatingWebhook returns the MutatingWebhook used for this CRD.
+func (m *Mutator) MutatingWebhook(clientConfig admissionregistrationv1.WebhookClientConfig) *admissionregistrationv1.MutatingWebhook {
+	mutatingWebhook := admission.NewDefaultMutationWebhook(m, clientConfig, admissionregistrationv1.NamespacedScope)
+	mutatingWebhook.SideEffects = admission.Ptr(admissionregistrationv1.SideEffectClassNoneOnDryRun)
+	return mutatingWebhook
+}
+
+// Admit is the entrypoint for the mutator. Admit will return an error if it unable to process the request.
+func (m *Mutator) Admit(request *admission.Request) (*admissionv1.AdmissionResponse, error) {
 	if request.DryRun != nil && *request.DryRun {
-		response.Allowed = true
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Allowed: true,
+		}, nil
 	}
 
 	listTrace := trace.New("secret Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
-	defer listTrace.LogIfLong(2 * time.Second)
+	defer listTrace.LogIfLong(admission.SlowTraceDuration)
 
-	secret, err := secretObject(request)
+	secret, err := objectsv1.SecretFromRequest(&request.AdmissionRequest)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if secret.Type != "provisioning.cattle.io/cloud-credential" {
-		response.Allowed = true
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Allowed: true,
+		}, nil
 	}
 
 	logrus.Debugf("[secret-mutation] adding creatorID %v to secret: %v", request.UserInfo.Username, secret.Name)
@@ -47,20 +70,10 @@ func (m *mutator) Admit(response *webhook.Response, request *webhook.Request) er
 	}
 
 	newSecret.Annotations[auth.CreatorIDAnn] = request.UserInfo.Username
+	response := &admissionv1.AdmissionResponse{}
 	if err := patch.CreatePatch(request.Object.Raw, newSecret, response); err != nil {
-		return fmt.Errorf("failed to create patch: %w", err)
+		return nil, fmt.Errorf("failed to create patch: %w", err)
 	}
 	response.Allowed = true
-	return nil
-}
-
-func secretObject(request *webhook.Request) (*v1.Secret, error) {
-	var secret runtime.Object
-	var err error
-	if request.Operation == admissionv1.Delete {
-		secret, err = request.DecodeOldObject()
-	} else {
-		secret, err = request.DecodeObject()
-	}
-	return secret.(*v1.Secret), err
+	return response, nil
 }

pkg/resources/validation/feature/feature.go
@@ -3,42 +3,67 @@ package feature
 import (
 	"fmt"
 	"net/http"
-	"time"
 
+	"github.com/rancher/webhook/pkg/admission"
 	objectsv3 "github.com/rancher/webhook/pkg/generated/objects/management.cattle.io/v3"
-	"github.com/rancher/wrangler/pkg/webhook"
+	admissionv1 "k8s.io/api/admission/v1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/utils/trace"
 )
 
-func NewValidator() webhook.Handler {
-	return &featureValidator{}
+var gvr = schema.GroupVersionResource{
+	Group:    "management.cattle.io",
+	Version:  "v3",
+	Resource: "features",
 }
 
-type featureValidator struct{}
+// Validator for validating features.
+type Validator struct{}
 
-func (fv *featureValidator) Admit(response *webhook.Response, request *webhook.Request) error {
+// GVR returns the GroupVersionKind for this CRD.
+func (v *Validator) GVR() schema.GroupVersionResource {
+	return gvr
+}
+
+// Operations returns list of operations handled by this validator.
+func (v *Validator) Operations() []admissionregistrationv1.OperationType {
+	return []admissionregistrationv1.OperationType{admissionregistrationv1.Update}
+}
+
+// ValidatingWebhook returns the ValidatingWebhook used for this CRD.
+func (v *Validator) ValidatingWebhook(clientConfig admissionregistrationv1.WebhookClientConfig) *admissionregistrationv1.ValidatingWebhook {
+	valWebhook := admission.NewDefaultValidationWebhook(v, clientConfig, admissionregistrationv1.ClusterScope)
+	valWebhook.FailurePolicy = admission.Ptr(admissionregistrationv1.Ignore)
+	return valWebhook
+}
+
+// Admit handles the webhook admission request sent to this webhook.
+func (v *Validator) Admit(request *admission.Request) (*admissionv1.AdmissionResponse, error) {
 	listTrace := trace.New("featureValidator Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
-	defer listTrace.LogIfLong(2 * time.Second)
+	defer listTrace.LogIfLong(admission.SlowTraceDuration)
 
 	oldFeature, newFeature, err := objectsv3.FeatureOldAndNewFromRequest(&request.AdmissionRequest)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if !isValidFeatureValue(newFeature.Status.LockedValue, oldFeature.Spec.Value, newFeature.Spec.Value) {
-		response.Result = &metav1.Status{
-			Status:  "Failure",
-			Message: fmt.Sprintf("feature flag cannot be changed from current value: %v", *newFeature.Status.LockedValue),
-			Reason:  metav1.StatusReasonInvalid,
-			Code:    http.StatusBadRequest,
-		}
-		response.Allowed = false
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Result: &metav1.Status{
+				Status:  "Failure",
+				Message: fmt.Sprintf("feature flag cannot be changed from current value: %v", *newFeature.Status.LockedValue),
+				Reason:  metav1.StatusReasonInvalid,
+				Code:    http.StatusBadRequest,
+			},
+			Allowed: false,
+		}, nil
 	}
 
-	response.Allowed = true
-	return nil
+	return &admissionv1.AdmissionResponse{
+		Allowed: true,
+	}, nil
 }
 
 // isValidFeatureValue checks that desired value does not change value on spec unless lockedValue

pkg/resources/validation/globalrole/globalrole.go
@@ -2,57 +2,87 @@ package globalrole
 
 import (
 	"net/http"
-	"time"
 
+	"github.com/rancher/webhook/pkg/admission"
 	"github.com/rancher/webhook/pkg/auth"
 	objectsv3 "github.com/rancher/webhook/pkg/generated/objects/management.cattle.io/v3"
-	"github.com/rancher/wrangler/pkg/webhook"
+	admissionv1 "k8s.io/api/admission/v1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/kubernetes/pkg/registry/rbac/validation"
 	"k8s.io/utils/trace"
 )
 
-func NewValidator(resolver validation.AuthorizationRuleResolver) webhook.Handler {
-	return &globalRoleValidator{
+var gvr = schema.GroupVersionResource{
+	Group:    "management.cattle.io",
+	Version:  "v3",
+	Resource: "globalroles",
+}
+
+// NewValidator returns a new validator used for validation globalRoles.
+func NewValidator(resolver validation.AuthorizationRuleResolver) *Validator {
+	return &Validator{
 		resolver: resolver,
 	}
 }
 
-type globalRoleValidator struct {
+// Validator implements admission.ValidatingAdmissionHandler
+type Validator struct {
 	resolver validation.AuthorizationRuleResolver
 }
 
-func (grv *globalRoleValidator) Admit(response *webhook.Response, request *webhook.Request) error {
+// GVR returns the GroupVersionKind for this CRD.
+func (v *Validator) GVR() schema.GroupVersionResource {
+	return gvr
+}
+
+// Operations returns list of operations handled by this validator.
+func (v *Validator) Operations() []admissionregistrationv1.OperationType {
+	return []admissionregistrationv1.OperationType{admissionregistrationv1.Update, admissionregistrationv1.Create}
+}
+
+// ValidatingWebhook returns the ValidatingWebhook used for this CRD.
+func (v *Validator) ValidatingWebhook(clientConfig admissionregistrationv1.WebhookClientConfig) *admissionregistrationv1.ValidatingWebhook {
+	return admission.NewDefaultValidationWebhook(v, clientConfig, admissionregistrationv1.ClusterScope)
+}
+
+// Admit is the entrypoint for the validator. Admit will return an error if it unable to process the request.
+// If this function is called without NewValidator(..) calls will panic.
+func (v *Validator) Admit(request *admission.Request) (*admissionv1.AdmissionResponse, error) {
 	listTrace := trace.New("globalRoleValidator Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
-	defer listTrace.LogIfLong(2 * time.Second)
+	defer listTrace.LogIfLong(admission.SlowTraceDuration)
 
 	newGR, err := objectsv3.GlobalRoleFromRequest(&request.AdmissionRequest)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// object is in the process of being deleted, so admit it
 	// this admits update operations that happen to remove finalizers
 	if newGR.DeletionTimestamp != nil {
-		response.Allowed = true
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Allowed: true,
+		}, nil
 	}
 
 	// ensure all PolicyRules have at least one verb, otherwise RBAC controllers may encounter issues when creating Roles and ClusterRoles
 	for _, rule := range newGR.Rules {
 		if len(rule.Verbs) == 0 {
-			response.Result = &metav1.Status{
-				Status:  "Failure",
-				Message: "GlobalRole.Rules: PolicyRules must have at least one verb",
-				Reason:  metav1.StatusReasonBadRequest,
-				Code:    http.StatusBadRequest,
-			}
-			response.Allowed = false
-			return nil
+			return &admissionv1.AdmissionResponse{
+				Result: &metav1.Status{
+					Status:  "Failure",
+					Message: "GlobalRole.Rules: PolicyRules must have at least one verb",
+					Reason:  metav1.StatusReasonBadRequest,
+					Code:    http.StatusBadRequest,
+				},
+				Allowed: false,
+			}, nil
 		}
 	}
 
-	auth.SetEscalationResponse(response, auth.ConfirmNoEscalation(request, newGR.Rules, "", grv.resolver))
+	response := &admissionv1.AdmissionResponse{}
+	auth.SetEscalationResponse(response, auth.ConfirmNoEscalation(request, newGR.Rules, "", v.resolver))
 
-	return nil
+	return response, nil
 }

rancher/webhook

pkg/resources/mutation/machineconfigs/machineconfigs.go
@@ -2,37 +2,61 @@ package machineconfigs
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/rancher/webhook/pkg/generated/objects/core/unstructured"
+	"github.com/rancher/webhook/pkg/admission"
+	v1 "github.com/rancher/webhook/pkg/generated/objects/core/v1"
 	"github.com/rancher/webhook/pkg/resources/mutation"
-	"github.com/rancher/wrangler/pkg/webhook"
+	admissionv1 "k8s.io/api/admission/v1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/utils/trace"
 )
 
-func NewMutator() webhook.Handler {
-	return &mutator{}
+var gvr = schema.GroupVersionResource{
+	Group:    "rke-machine-config.cattle.io",
+	Version:  "v1",
+	Resource: "*",
 }
 
-type mutator struct {
+// Mutator implements admission.MutatingAdmissionWebhook.
+type Mutator struct{}
+
+// GVR returns the GroupVersionKind for this CRD.
+func (m *Mutator) GVR() schema.GroupVersionResource {
+	return gvr
 }
 
-func (m *mutator) Admit(response *webhook.Response, request *webhook.Request) error {
+// Operations returns list of operations handled by this mutator.
+func (m *Mutator) Operations() []admissionregistrationv1.OperationType {
+	return []admissionregistrationv1.OperationType{admissionregistrationv1.Create}
+}
+
+// MutatingWebhook returns the MutatingWebhook used for this CRD.
+func (m *Mutator) MutatingWebhook(clientConfig admissionregistrationv1.WebhookClientConfig) *admissionregistrationv1.MutatingWebhook {
+	mutatingWebhook := admission.NewDefaultMutatingWebhook(m, clientConfig, admissionregistrationv1.NamespacedScope)
+	mutatingWebhook.SideEffects = admission.Ptr(admissionregistrationv1.SideEffectClassNoneOnDryRun)
+	return mutatingWebhook
+}
+
+// Admit is the entrypoint for the mutator. Admit will return an error if it unable to process the request.
+func (m *Mutator) Admit(request *admission.Request) (*admissionv1.AdmissionResponse, error) {
 	if request.DryRun != nil && *request.DryRun {
-		response.Allowed = true
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Allowed: true,
+		}, nil
 	}
 
 	listTrace := trace.New("machine config Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
-	defer listTrace.LogIfLong(2 * time.Second)
+	defer listTrace.LogIfLong(admission.SlowTraceDuration)
 
-	config, err := unstructured.UnstructuredFromRequest(&request.AdmissionRequest)
+	config, err := v1.UnstructuredFromRequest(&request.AdmissionRequest)
 	if err != nil {
-		return fmt.Errorf("failed to get object from request: %w", err)
+		return nil, fmt.Errorf("failed to get object from request: %w", err)
 	}
+	response := &admissionv1.AdmissionResponse{}
 	if err := mutation.SetCreatorIDAnnotation(request, response, request.Object, config.DeepCopy()); err != nil {
-		return fmt.Errorf("failed to set creatorIDAnnotation: %w", err)
+		return nil, fmt.Errorf("failed to set creatorIDAnnotation: %w", err)
 	}
 	response.Allowed = true
-	return nil
+	return response, nil
 }

pkg/resources/mutation/secret/secret.go
@@ -2,41 +2,64 @@ package secret
 
 import (
 	"fmt"
-	"time"
 
+	"github.com/rancher/webhook/pkg/admission"
 	"github.com/rancher/webhook/pkg/auth"
+	objectsv1 "github.com/rancher/webhook/pkg/generated/objects/core/v1"
 	"github.com/rancher/webhook/pkg/patch"
-	"github.com/rancher/wrangler/pkg/webhook"
 	"github.com/sirupsen/logrus"
 	admissionv1 "k8s.io/api/admission/v1"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/utils/trace"
 )
 
-func NewMutator() webhook.Handler {
-	return &mutator{}
+var gvr = schema.GroupVersionResource{
+	Group:    "",
+	Version:  "v1",
+	Resource: "secrets",
 }
 
-type mutator struct{}
+// Mutator implements admission.MutatingAdmissionWebhook.
+type Mutator struct{}
 
-func (m *mutator) Admit(response *webhook.Response, request *webhook.Request) error {
+// GVR returns the GroupVersionKind for this CRD.
+func (m *Mutator) GVR() schema.GroupVersionResource {
+	return gvr
+}
+
+// Operations returns list of operations handled by this mutator.
+func (m *Mutator) Operations() []admissionregistrationv1.OperationType {
+	return []admissionregistrationv1.OperationType{admissionregistrationv1.Create}
+}
+
+// MutatingWebhook returns the MutatingWebhook used for this CRD.
+func (m *Mutator) MutatingWebhook(clientConfig admissionregistrationv1.WebhookClientConfig) *admissionregistrationv1.MutatingWebhook {
+	mutatingWebhook := admission.NewDefaultMutatingWebhook(m, clientConfig, admissionregistrationv1.NamespacedScope)
+	mutatingWebhook.SideEffects = admission.Ptr(admissionregistrationv1.SideEffectClassNoneOnDryRun)
+	return mutatingWebhook
+}
+
+// Admit is the entrypoint for the mutator. Admit will return an error if it unable to process the request.
+func (m *Mutator) Admit(request *admission.Request) (*admissionv1.AdmissionResponse, error) {
 	if request.DryRun != nil && *request.DryRun {
-		response.Allowed = true
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Allowed: true,
+		}, nil
 	}
 
 	listTrace := trace.New("secret Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
-	defer listTrace.LogIfLong(2 * time.Second)
+	defer listTrace.LogIfLong(admission.SlowTraceDuration)
 
-	secret, err := secretObject(request)
+	secret, err := objectsv1.SecretFromRequest(&request.AdmissionRequest)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if secret.Type != "provisioning.cattle.io/cloud-credential" {
-		response.Allowed = true
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Allowed: true,
+		}, nil
 	}
 
 	logrus.Debugf("[secret-mutation] adding creatorID %v to secret: %v", request.UserInfo.Username, secret.Name)
@@ -47,20 +70,10 @@ func (m *mutator) Admit(response *webhook.Response, request *webhook.Request) er
 	}
 
 	newSecret.Annotations[auth.CreatorIDAnn] = request.UserInfo.Username
+	response := &admissionv1.AdmissionResponse{}
 	if err := patch.CreatePatch(request.Object.Raw, newSecret, response); err != nil {
-		return fmt.Errorf("failed to create patch: %w", err)
+		return nil, fmt.Errorf("failed to create patch: %w", err)
 	}
 	response.Allowed = true
-	return nil
-}
-
-func secretObject(request *webhook.Request) (*v1.Secret, error) {
-	var secret runtime.Object
-	var err error
-	if request.Operation == admissionv1.Delete {
-		secret, err = request.DecodeOldObject()
-	} else {
-		secret, err = request.DecodeObject()
-	}
-	return secret.(*v1.Secret), err
+	return response, nil
 }

pkg/resources/validation/feature/feature.go
@@ -3,42 +3,67 @@ package feature
 import (
 	"fmt"
 	"net/http"
-	"time"
 
+	"github.com/rancher/webhook/pkg/admission"
 	objectsv3 "github.com/rancher/webhook/pkg/generated/objects/management.cattle.io/v3"
-	"github.com/rancher/wrangler/pkg/webhook"
+	admissionv1 "k8s.io/api/admission/v1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/utils/trace"
 )
 
-func NewValidator() webhook.Handler {
-	return &featureValidator{}
+var gvr = schema.GroupVersionResource{
+	Group:    "management.cattle.io",
+	Version:  "v3",
+	Resource: "features",
 }
 
-type featureValidator struct{}
+// Validator for validating features.
+type Validator struct{}
 
-func (fv *featureValidator) Admit(response *webhook.Response, request *webhook.Request) error {
+// GVR returns the GroupVersionKind for this CRD.
+func (v *Validator) GVR() schema.GroupVersionResource {
+	return gvr
+}
+
+// Operations returns list of operations handled by this validator.
+func (v *Validator) Operations() []admissionregistrationv1.OperationType {
+	return []admissionregistrationv1.OperationType{admissionregistrationv1.Update}
+}
+
+// ValidatingWebhook returns the ValidatingWebhook used for this CRD.
+func (v *Validator) ValidatingWebhook(clientConfig admissionregistrationv1.WebhookClientConfig) *admissionregistrationv1.ValidatingWebhook {
+	valWebhook := admission.NewDefaultValidatingWebhook(v, clientConfig, admissionregistrationv1.ClusterScope)
+	valWebhook.FailurePolicy = admission.Ptr(admissionregistrationv1.Ignore)
+	return valWebhook
+}
+
+// Admit handles the webhook admission request sent to this webhook.
+func (v *Validator) Admit(request *admission.Request) (*admissionv1.AdmissionResponse, error) {
 	listTrace := trace.New("featureValidator Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
-	defer listTrace.LogIfLong(2 * time.Second)
+	defer listTrace.LogIfLong(admission.SlowTraceDuration)
 
 	oldFeature, newFeature, err := objectsv3.FeatureOldAndNewFromRequest(&request.AdmissionRequest)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if !isValidFeatureValue(newFeature.Status.LockedValue, oldFeature.Spec.Value, newFeature.Spec.Value) {
-		response.Result = &metav1.Status{
-			Status:  "Failure",
-			Message: fmt.Sprintf("feature flag cannot be changed from current value: %v", *newFeature.Status.LockedValue),
-			Reason:  metav1.StatusReasonInvalid,
-			Code:    http.StatusBadRequest,
-		}
-		response.Allowed = false
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Result: &metav1.Status{
+				Status:  "Failure",
+				Message: fmt.Sprintf("feature flag cannot be changed from current value: %v", *newFeature.Status.LockedValue),
+				Reason:  metav1.StatusReasonInvalid,
+				Code:    http.StatusBadRequest,
+			},
+			Allowed: false,
+		}, nil
 	}
 
-	response.Allowed = true
-	return nil
+	return &admissionv1.AdmissionResponse{
+		Allowed: true,
+	}, nil
 }
 
 // isValidFeatureValue checks that desired value does not change value on spec unless lockedValue

pkg/resources/validation/globalrole/globalrole.go
@@ -2,57 +2,87 @@ package globalrole
 
 import (
 	"net/http"
-	"time"
 
+	"github.com/rancher/webhook/pkg/admission"
 	"github.com/rancher/webhook/pkg/auth"
 	objectsv3 "github.com/rancher/webhook/pkg/generated/objects/management.cattle.io/v3"
-	"github.com/rancher/wrangler/pkg/webhook"
+	admissionv1 "k8s.io/api/admission/v1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/kubernetes/pkg/registry/rbac/validation"
 	"k8s.io/utils/trace"
 )
 
-func NewValidator(resolver validation.AuthorizationRuleResolver) webhook.Handler {
-	return &globalRoleValidator{
+var gvr = schema.GroupVersionResource{
+	Group:    "management.cattle.io",
+	Version:  "v3",
+	Resource: "globalroles",
+}
+
+// NewValidator returns a new validator used for validation globalRoles.
+func NewValidator(resolver validation.AuthorizationRuleResolver) *Validator {
+	return &Validator{
 		resolver: resolver,
 	}
 }
 
-type globalRoleValidator struct {
+// Validator implements admission.ValidatingAdmissionHandler
+type Validator struct {
 	resolver validation.AuthorizationRuleResolver
 }
 
-func (grv *globalRoleValidator) Admit(response *webhook.Response, request *webhook.Request) error {
+// GVR returns the GroupVersionKind for this CRD.
+func (v *Validator) GVR() schema.GroupVersionResource {
+	return gvr
+}
+
+// Operations returns list of operations handled by this validator.
+func (v *Validator) Operations() []admissionregistrationv1.OperationType {
+	return []admissionregistrationv1.OperationType{admissionregistrationv1.Update, admissionregistrationv1.Create}
+}
+
+// ValidatingWebhook returns the ValidatingWebhook used for this CRD.
+func (v *Validator) ValidatingWebhook(clientConfig admissionregistrationv1.WebhookClientConfig) *admissionregistrationv1.ValidatingWebhook {
+	return admission.NewDefaultValidatingWebhook(v, clientConfig, admissionregistrationv1.ClusterScope)
+}
+
+// Admit is the entrypoint for the validator. Admit will return an error if it unable to process the request.
+// If this function is called without NewValidator(..) calls will panic.
+func (v *Validator) Admit(request *admission.Request) (*admissionv1.AdmissionResponse, error) {
 	listTrace := trace.New("globalRoleValidator Admit", trace.Field{Key: "user", Value: request.UserInfo.Username})
-	defer listTrace.LogIfLong(2 * time.Second)
+	defer listTrace.LogIfLong(admission.SlowTraceDuration)
 
 	newGR, err := objectsv3.GlobalRoleFromRequest(&request.AdmissionRequest)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// object is in the process of being deleted, so admit it
 	// this admits update operations that happen to remove finalizers
 	if newGR.DeletionTimestamp != nil {
-		response.Allowed = true
-		return nil
+		return &admissionv1.AdmissionResponse{
+			Allowed: true,
+		}, nil
 	}
 
 	// ensure all PolicyRules have at least one verb, otherwise RBAC controllers may encounter issues when creating Roles and ClusterRoles
 	for _, rule := range newGR.Rules {
 		if len(rule.Verbs) == 0 {
-			response.Result = &metav1.Status{
-				Status:  "Failure",
-				Message: "GlobalRole.Rules: PolicyRules must have at least one verb",
-				Reason:  metav1.StatusReasonBadRequest,
-				Code:    http.StatusBadRequest,
-			}
-			response.Allowed = false
-			return nil
+			return &admissionv1.AdmissionResponse{
+				Result: &metav1.Status{
+					Status:  "Failure",
+					Message: "GlobalRole.Rules: PolicyRules must have at least one verb",
+					Reason:  metav1.StatusReasonBadRequest,
+					Code:    http.StatusBadRequest,
+				},
+				Allowed: false,
+			}, nil
 		}
 	}
 
-	auth.SetEscalationResponse(response, auth.ConfirmNoEscalation(request, newGR.Rules, "", grv.resolver))
+	response := &admissionv1.AdmissionResponse{}
+	auth.SetEscalationResponse(response, auth.ConfirmNoEscalation(request, newGR.Rules, "", v.resolver))
 
-	return nil
+	return response, nil
 }