8aac201f8a8c59dea5d993659be712a358ffdd2b
Eric Promislow <epromislow@suse.com>
2024-07-08 10:26:20 -0700

    v0.4 GHA: Install rancher against an up-to-date chart.

--- .github/workflows/scripts/start-rancher.sh
+++ .github/workflows/scripts/start-rancher.sh
@@ -22,4 +22,6 @@ helm upgrade --install cert-manager --namespace cert-manager cert-manager/cert-m
 # kubectl get pods --namespace cert-manager
 kubectl rollout status --namespace cert-manager deploy/cert-manager --timeout 1m
 
-helm upgrade --install rancher rancher-latest/rancher --namespace cattle-system --set hostname=localhost --wait --timeout=10m --create-namespace --version "$VERSION"
+# Chart based
+
+helm upgrade --install rancher "$CHART_PATH" --namespace cattle-system --set replicas=1 --set hostname=localhost --wait --timeout=10m --create-namespace --version "$VERSION" --set rancherImage=rancher/rancher --set rancherImageTag="$RANCHER_IMAGE_TAG"
