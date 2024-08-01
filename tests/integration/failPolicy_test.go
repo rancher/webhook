d7efbfeac73d23050174a9545fc686e272d1bacd
Peter Matseykanets <peter.matseykanets@suse.com>
2024-08-01 12:22:08 -0400

    Add unit and integration tests

--- tests/integration/failPolicy_test.go
+++ tests/integration/failPolicy_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/rancher/lasso/pkg/client"
 	v3 "github.com/rancher/rancher/pkg/apis/cluster.cattle.io/v3"
+	mgmtv3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
 	provisioningv1 "github.com/rancher/rancher/pkg/apis/provisioning.cattle.io/v1"
 	"github.com/rancher/wrangler/v2/pkg/gvk"
 	"github.com/rancher/wrangler/v2/pkg/kubeconfig"
@@ -43,21 +44,11 @@ func (m *FailurePolicySuite) SetupSuite() {
 	schemes.Register(provisioningv1.AddToScheme)
 	schemes.Register(corev1.AddToScheme)
 }
+func (m *FailurePolicySuite) SetupTest() {
+	m.checkWebhookIsDown()
+}
 
-func (m *FailurePolicySuite) TestNamespaceFail() {
-	const testNamespace = "test-namespace"
-	validCreateObj := &corev1.Namespace{
-		ObjectMeta: v1.ObjectMeta{
-			Name: testNamespace,
-		},
-	}
-
-	objGVK, err := gvk.Get(validCreateObj)
-	m.Require().NoError(err, "failed to get GVK")
-
-	client, err := m.clientFactory.ForKind(objGVK)
-	m.Require().NoError(err, "Failed to create client")
-
+func (m *FailurePolicySuite) checkWebhookIsDown() {
 	podGVK := schema.GroupVersionKind{
 		Group:   "",
 		Version: "v1",
@@ -72,14 +63,29 @@ func (m *FailurePolicySuite) TestNamespaceFail() {
 	pods := corev1.PodList{}
 	podClient.List(context.Background(), "cattle-system", &pods, listOpts)
 	m.Require().Equal(0, len(pods.Items), "Test can not run while rancher-webhook pods are still running")
+}
+
+func (m *FailurePolicySuite) TestNamespaceFail() {
+	const name = "test-namespace"
+	validCreateObj := &corev1.Namespace{
+		ObjectMeta: v1.ObjectMeta{
+			Name: name,
+		},
+	}
+
+	objGVK, err := gvk.Get(validCreateObj)
+	m.Require().NoError(err, "failed to get GVK")
+
+	client, err := m.clientFactory.ForKind(objGVK)
+	m.Require().NoError(err, "Failed to create client")
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
 	defer cancel()
 	err = client.Create(ctx, "", validCreateObj, nil, v1.CreateOptions{})
 	m.Require().True(errors.IsInternalError(err), "Webhook should fail with service unavailable when the webhook is down instead we received :%v", err)
 
-	// attempt to clean up namespace if the create went through
-	defer client.Delete(ctx, "", testNamespace, v1.DeleteOptions{})
+	// Attempt to clean up the namespace if the create went through.
+	defer client.Delete(ctx, "", name, v1.DeleteOptions{})
 
 	validCreateObj.Name = "default"
 	err = client.Update(ctx, "", validCreateObj, nil, v1.UpdateOptions{})
@@ -91,4 +97,34 @@ func (m *FailurePolicySuite) TestNamespaceFail() {
 
 	err = client.Update(ctx, "", validCreateObj, nil, v1.UpdateOptions{})
 	m.Require().True(errors.IsInternalError(err), "Webhook should fail to update kube-system namespace with service unavailable when the webhook is down instead we received :%v", err)
+}
+
+func (m *FailurePolicySuite) TestSettingSucceed() {
+	const name = "new-setting"
+	createObj := &mgmtv3.Setting{
+		ObjectMeta: v1.ObjectMeta{
+			Name: name,
+		},
+	}
+	objGVK, err := gvk.Get(createObj)
+	m.Require().NoError(err, "failed to get GVK")
+
+	client, err := m.clientFactory.ForKind(objGVK)
+	m.Require().NoError(err, "Failed to create client")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	err = client.Create(ctx, "", createObj, nil, v1.CreateOptions{})
+	m.Require().NoError(err)
+
+	// Attempt to clean up the setting if the create went through.
+	defer client.Delete(ctx, "", name, v1.DeleteOptions{})
+
+	updateObj := &mgmtv3.Setting{}
+	err = client.Get(ctx, "", name, updateObj, v1.GetOptions{})
+	m.Require().NoError(err)
+	updateObj.Value = "new-value"
+
+	err = client.Update(ctx, "", updateObj, updateObj, v1.UpdateOptions{})
+	m.Require().NoError(err)
 }
