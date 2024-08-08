d5308b7e81197f53bee0368d4d789398c47a279e
Eric Promislow <epromislow@suse.com>
2024-08-08 10:40:20 -0700

    Verify the ClusterProxyConfig obj is created in the integration test.
    Sometimes the test to verify only one CPC can be created at a time fails
    when verifying that the attempted second-object creation returns an
    error.  So we watch for that object to be created, and then continue the test.

--- tests/integration/clusterProxyConfig_test.go
+++ tests/integration/clusterProxyConfig_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	v3 "github.com/rancher/rancher/pkg/apis/management.cattle.io/v3"
+	"github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -17,7 +18,8 @@ func (m *IntegrationSuite) TestClusterProxyConfig() {
 		Version: "v3",
 		Kind:    "ClusterProxyConfig",
 	}
-	validCreateObj := getObjectToCreate("testclusterproxyconfig")
+	cpcName := "testclusterproxyconfig"
+	validCreateObj := getObjectToCreate(cpcName)
 	client, err := m.clientFactory.ForKind(objGVK)
 	m.Require().NoError(err, "Failed to create client")
 	result := &v3.ClusterProxyConfig{}
@@ -26,6 +28,36 @@ func (m *IntegrationSuite) TestClusterProxyConfig() {
 	// Creating the first CPC should succeed
 	err = client.Create(ctx, validCreateObj.Namespace, validCreateObj, result, metav1.CreateOptions{})
 	m.NoError(err, "Error returned during the creation of a valid clusterProxyConfig")
+
+	// Verify the API knows about the first object before we try to create a second one.
+	// Wait 2 minutes for the object to be created (should be more than ample time)
+	timeout := int64(5 * time.Minute)
+	listOptions := metav1.ListOptions{
+		Watch:          true,
+		TimeoutSeconds: &timeout,
+	}
+	watcher, err := client.Watch(ctx, validCreateObj.Namespace, listOptions)
+	m.NoError(err, "Error returned trying to watch the clusterProxyConfig")
+	if watcher != nil {
+		// If `m.NoError` fails, code still keeps running and we have a nil watcher
+		defer watcher.Stop()
+		for {
+			receivedEvent, ok := <-watcher.ResultChan()
+			if !ok {
+				logrus.Infof("Got a non-ok event from the watcher; breaking")
+				break
+			}
+			if receivedEvent.Object.GetObjectKind().GroupVersionKind().Kind == "ClusterProxyConfig" {
+				err = client.Get(ctx, validCreateObj.Namespace, cpcName, result, metav1.GetOptions{})
+				if err == nil {
+					break
+				}
+				logrus.Infof("client.Get(...) => err %s", err)
+				break
+			}
+		}
+	}
+
 	secondCPC := getObjectToCreate("anotherclusterproxyconfig")
 	// Attempting to create another CPC in the same namespace should fail
 	err = client.Create(ctx, validCreateObj.Namespace, secondCPC, result, metav1.CreateOptions{})
