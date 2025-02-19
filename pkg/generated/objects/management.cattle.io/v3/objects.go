5485a1e23bbcd9da1cc44eb55bd0dea9aa2ca13f
Peter Matseykanets <peter.matseykanets@suse.com>
2025-02-18 23:39:51 -0500

    Validate LDAP/AD authconfigs

--- pkg/generated/objects/management.cattle.io/v3/objects.go
+++ pkg/generated/objects/management.cattle.io/v3/objects.go
@@ -643,3 +643,56 @@ func SettingFromRequest(request *admissionv1.AdmissionRequest) (*v3.Setting, err
 
 	return object, nil
 }
+
+// AuthConfigOldAndNewFromRequest gets the old and new AuthConfig objects, respectively, from the webhook request.
+// If the request is a Delete operation, then the new object is the zero value for AuthConfig.
+// Similarly, if the request is a Create operation, then the old object is the zero value for AuthConfig.
+func AuthConfigOldAndNewFromRequest(request *admissionv1.AdmissionRequest) (*v3.AuthConfig, *v3.AuthConfig, error) {
+	if request == nil {
+		return nil, nil, fmt.Errorf("nil request")
+	}
+
+	object := &v3.AuthConfig{}
+	oldObject := &v3.AuthConfig{}
+
+	if request.Operation != admissionv1.Delete {
+		err := json.Unmarshal(request.Object.Raw, object)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to unmarshal request object: %w", err)
+		}
+	}
+
+	if request.Operation == admissionv1.Create {
+		return oldObject, object, nil
+	}
+
+	err := json.Unmarshal(request.OldObject.Raw, oldObject)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to unmarshal request oldObject: %w", err)
+	}
+
+	return oldObject, object, nil
+}
+
+// AuthConfigFromRequest returns a AuthConfig object from the webhook request.
+// If the operation is a Delete operation, then the old object is returned.
+// Otherwise, the new object is returned.
+func AuthConfigFromRequest(request *admissionv1.AdmissionRequest) (*v3.AuthConfig, error) {
+	if request == nil {
+		return nil, fmt.Errorf("nil request")
+	}
+
+	object := &v3.AuthConfig{}
+	raw := request.Object.Raw
+
+	if request.Operation == admissionv1.Delete {
+		raw = request.OldObject.Raw
+	}
+
+	err := json.Unmarshal(raw, object)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal request object: %w", err)
+	}
+
+	return object, nil
+}
---
+++ pkg/resources/management.cattle.io/v3/authconfig/Authconfig.md
@@ -0,0 +1,8 @@
+
+## Validation Checks
+
+### Create and Update
+
+When LDAP or AD authconfig is are created or updated, the following common checks take place:
+
+- TBD.
