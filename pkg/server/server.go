be5af116aaf75f3111227cd3d9dae42079dbd72f
Chad Roberts <chad.roberts@suse.com>
2026-05-06 11:22:01 -0400

    replace dynamiclistener + secretHandler with file-based TLS
    Remove leader election, secretHandler, ensureWebhookConfiguration, and
    dynamiclistener dependency. The webhook now reads serving certs from
    mounted files (/tmp/k8s-webhook-server/serving-certs/) populated by
    needacert via a projected Secret volume. Cert rotation is handled by
    re-reading the files on each TLS handshake. WebhookConfiguration
    ownership has moved to the rancher-webhook Helm chart.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -12,50 +12,28 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-	"sync/atomic"
-	"time"
 
-	"github.com/google/uuid"
-	"github.com/rancher/dynamiclistener"
-	"github.com/rancher/dynamiclistener/server"
 	"github.com/rancher/webhook/pkg/admission"
 	"github.com/rancher/webhook/pkg/clients"
 	"github.com/rancher/webhook/pkg/health"
-	admissionregistration "github.com/rancher/wrangler/v3/pkg/generated/controllers/admissionregistration.k8s.io/v1"
 	"github.com/sirupsen/logrus"
-	v1 "k8s.io/api/admissionregistration/v1"
-	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/leaderelection"
-	"k8s.io/client-go/tools/leaderelection/resourcelock"
 )
 
 const (
-	serviceName             = "rancher-webhook"
-	namespace               = "cattle-system"
-	tlsName                 = "rancher-webhook.cattle-system.svc"
-	certName                = "cattle-webhook-tls"
-	caName                  = "cattle-webhook-ca"
 	validationPath          = "/v1/webhook/validation"
 	mutationPath            = "/v1/webhook/mutation"
-	clientPort              = int32(443)
 	webhookHTTPPort         = 0 // value of 0 indicates we do not want to use http.
 	defaultWebhookHTTPSPort = 9443
 	webhookPortEnvKey       = "CATTLE_PORT"
-	webhookURLEnvKey        = "CATTLE_WEBHOOK_URL"
+	webhookCertDirEnvKey    = "CATTLE_WEBHOOK_CERT_DIR"
+	defaultCertDir          = "/tmp/k8s-webhook-server/serving-certs"
 	allowedCNsEnv           = "ALLOWED_CNS"
 )
 
 var caFile = filepath.Join(os.TempDir(), "k8s-webhook-server", "client-ca", "ca.crt")
 
-// leaderFlag indicates whether this process is the elected leader.
-// Gate config mutation work on this to avoid concurrent writers.
-var leaderFlag atomic.Bool
-
-// tlsOpt option function applied to all webhook servers.
+// tlsOpt configures the TLS settings shared by the serving listener.
 var tlsOpt = func(config *tls.Config) {
 	config.MinVersion = tls.VersionTLS12
 	config.CipherSuites = []uint16{
@@ -76,13 +54,6 @@ func ListenAndServe(ctx context.Context, cfg *rest.Config, mcmEnabled bool) erro
 		return fmt.Errorf("failed to create a new client: %w", err)
 	}
 
-	if err = setCertificateExpirationDays(); err != nil {
-		// If this error occurs, certificate creation will still work. However, our override will likely not have worked.
-		// This will not affect functionality of the webhook, but users may have to perform the workaround:
-		// https://github.com/rancher/docs/issues/3637
-		logrus.Infof("[ListenAndServe] could not set certificate expiration days via environment variable: %v", err)
-	}
-
 	validators, err := Validation(clients)
 	if err != nil {
 		return err
@@ -92,49 +63,6 @@ func ListenAndServe(ctx context.Context, cfg *rest.Config, mcmEnabled bool) erro
 	if err != nil {
 		return err
 	}
-
-	k8sClient, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		return fmt.Errorf("failed to create kubernetes clientset: %w", err)
-	}
-	id := uuid.New().String()
-
-	lock := &resourcelock.LeaseLock{
-		LeaseMeta: metav1.ObjectMeta{
-			Name:      "rancher-webhook-leader",
-			Namespace: namespace,
-		},
-		Client: k8sClient.CoordinationV1(),
-		LockConfig: resourcelock.ResourceLockConfig{
-			Identity: id,
-		},
-	}
-
-	go leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
-		Lock:            lock,
-		LeaseDuration:   15 * time.Second,
-		RenewDeadline:   10 * time.Second,
-		RetryPeriod:     2 * time.Second,
-		ReleaseOnCancel: true,
-		Callbacks: leaderelection.LeaderCallbacks{
-			OnStartedLeading: func(_ context.Context) {
-				leaderFlag.Store(true)
-				clients.Core.Secret().Enqueue(namespace, caName)
-				logrus.Infof("[%s] elected leader: will manage webhook configurations", id)
-			},
-			OnStoppedLeading: func() {
-				leaderFlag.Store(false)
-				logrus.Infof("[%s] lost leadership: will stop managing webhook configurations", id)
-			},
-			OnNewLeader: func(identity string) {
-				if identity == id {
-					logrus.Infof("[%s] I am the new leader", id)
-				} else {
-					logrus.Infof("[%s] observed new leader: %s", id, identity)
-				}
-			},
-		},
-	})
 
 	if err = listenAndServe(ctx, clients, validators, mutators); err != nil {
 		return err
@@ -147,22 +75,10 @@ func ListenAndServe(ctx context.Context, cfg *rest.Config, mcmEnabled bool) erro
 	return nil
 }
 
-// By default, dynamiclistener sets newly signed certificates to expire after 365 days. Since the
-// self-signed certificate for webhook does not need to be rotated, we increase expiration time
-// beyond relevance. In this case, that's 3650 days (10 years).
-func setCertificateExpirationDays() error {
-	certExpirationDaysKey := "CATTLE_NEW_SIGNED_CERT_EXPIRATION_DAYS"
-	if os.Getenv(certExpirationDaysKey) == "" {
-		return os.Setenv(certExpirationDaysKey, "3650")
-	}
-	return nil
-}
-
 func listenAndServe(ctx context.Context, clients *clients.Clients, validators []admission.ValidatingAdmissionHandler, mutators []admission.MutatingAdmissionHandler) (rErr error) {
 	router := http.NewServeMux()
 	errChecker := health.NewErrorChecker("config-applied")
 	health.RegisterHealthCheckers(router, errChecker)
-	errChecker.Store(errors.New("webhook configuration not yet applied"))
 
 	logrus.Debug("Creating Webhook routes")
 	for _, webhook := range validators {
@@ -178,15 +94,6 @@ func listenAndServe(ctx context.Context, clients *clients.Clients, validators []
 
 	routerHandler := certAuth()(router)
 
-	handler := &secretHandler{
-		validators:           validators,
-		mutators:             mutators,
-		errChecker:           errChecker,
-		validatingController: clients.Admission.ValidatingWebhookConfiguration(),
-		mutatingController:   clients.Admission.MutatingWebhookConfiguration(),
-	}
-	clients.Core.Secret().OnChange(ctx, "secrets", handler.sync)
-
 	defer func() {
 		if rErr != nil {
 			return
@@ -194,8 +101,6 @@ func listenAndServe(ctx context.Context, clients *clients.Clients, validators []
 		rErr = clients.Start(ctx)
 	}()
 
-	tlsConfig := &tls.Config{}
-	tlsOpt(tlsConfig)
 	webhookHTTPSPort := defaultWebhookHTTPSPort
 	if portStr := os.Getenv(webhookPortEnvKey); portStr != "" {
 		var err error
@@ -204,142 +109,57 @@ func listenAndServe(ctx context.Context, clients *clients.Clients, validators []
 			return fmt.Errorf("failed to decode webhook port value '%s': %w", portStr, err)
 		}
 	}
-	return server.ListenAndServe(ctx, webhookHTTPSPort, webhookHTTPPort, routerHandler, &server.ListenOpts{
-		Secrets:       clients.Core.Secret(),
-		CertNamespace: namespace,
-		CertName:      certName,
-		CAName:        caName,
-		TLSListenerConfig: dynamiclistener.Config{
-			SANs: []string{
-				tlsName,
-			},
-			FilterCN:  dynamiclistener.OnlyAllow(tlsName),
-			TLSConfig: tlsConfig,
-		},
-		DisplayServerLogs:       true,
-		IgnoreTLSHandshakeError: true,
-	})
-}
 
-type secretHandler struct {
-	validators           []admission.ValidatingAdmissionHandler
-	mutators             []admission.MutatingAdmissionHandler
-	errChecker           *health.ErrorChecker
-	validatingController admissionregistration.ValidatingWebhookConfigurationClient
-	mutatingController   admissionregistration.MutatingWebhookConfigurationClient
-}
+	certDir := defaultCertDir
+	if dir := os.Getenv(webhookCertDirEnvKey); dir != "" {
+		certDir = dir
+	}
+	certPath := filepath.Join(certDir, "tls.crt")
+	keyPath := filepath.Join(certDir, "tls.key")
 
-// sync updates the validating admission configuration whenever the TLS cert changes.
-// Only the elected leader performs the updates, followers are a no-op.
-func (s *secretHandler) sync(_ string, secret *corev1.Secret) (*corev1.Secret, error) {
-	// The leader is responsible for applying the webhook configuration.
-	// Follower pods are only responsible for serving traffic and can be marked as healthy once the certificates are generated.
-	if !leaderFlag.Load() {
-		s.errChecker.Store(nil)
-		return nil, nil
+	tlsConfig := &tls.Config{
+		GetCertificate: certReloader(certPath, keyPath),
+	}
+	tlsOpt(tlsConfig)
+
+	server := &http.Server{
+		Addr:      fmt.Sprintf(":%d", webhookHTTPSPort),
+		Handler:   routerHandler,
+		TLSConfig: tlsConfig,
 	}
 
-	if secret == nil || secret.Name != caName || secret.Namespace != namespace || len(secret.Data[corev1.TLSCertKey]) == 0 {
-		return nil, nil
-	}
-
-	logrus.Info("Applying webhook config")
-
-	validationClientConfig := v1.WebhookClientConfig{
-		Service: &v1.ServiceReference{
-			Namespace: namespace,
-			Name:      serviceName,
-			Path:      admission.Ptr(validationPath),
-			Port:      admission.Ptr(clientPort),
-		},
-		CABundle: secret.Data[corev1.TLSCertKey],
-	}
-
-	mutationClientConfig := v1.WebhookClientConfig{
-		Service: &v1.ServiceReference{
-			Namespace: namespace,
-			Name:      serviceName,
-			Path:      admission.Ptr(mutationPath),
-			Port:      admission.Ptr(clientPort),
-		},
-		CABundle: secret.Data[corev1.TLSCertKey],
-	}
-	if devURL, ok := os.LookupEnv(webhookURLEnvKey); ok {
-		validationURL := devURL + validationPath
-		mutationURL := devURL + mutationPath
-		validationClientConfig = v1.WebhookClientConfig{
-			URL: &validationURL,
+	go func() {
+		<-ctx.Done()
+		shutdownCtx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			logrus.Warnf("webhook server shutdown returned error: %v", err)
 		}
-		mutationClientConfig = v1.WebhookClientConfig{
-			URL: &mutationURL,
-		}
-	}
-	validatingWebhooks := make([]v1.ValidatingWebhook, 0, len(s.validators))
-	for _, webhook := range s.validators {
-		validatingWebhooks = append(validatingWebhooks, webhook.ValidatingWebhook(validationClientConfig)...)
-	}
-	mutatingWebhooks := make([]v1.MutatingWebhook, 0, len(s.mutators))
-	for _, webhook := range s.mutators {
-		mutatingWebhooks = append(mutatingWebhooks, webhook.MutatingWebhook(mutationClientConfig)...)
-	}
-	validatingConfig := &v1.ValidatingWebhookConfiguration{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "rancher.cattle.io",
-		},
-		Webhooks: validatingWebhooks,
-	}
-	mutatingConfig := &v1.MutatingWebhookConfiguration{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "rancher.cattle.io",
-		},
-		Webhooks: mutatingWebhooks,
-	}
-	err := s.ensureWebhookConfiguration(validatingConfig, mutatingConfig)
-	if err != nil {
-		logrus.Errorf("Failed to ensure configuration: %s", err.Error())
-	}
+	}()
 
-	s.errChecker.Store(err)
-	return secret, err
-
-}
-
-// ensureWebhookConfiguration creates or updates the current validating and mutating webhook configuration to have the desired webhook.
-func (s *secretHandler) ensureWebhookConfiguration(validatingConfig *v1.ValidatingWebhookConfiguration, mutatingConfig *v1.MutatingWebhookConfiguration) error {
-
-	currValidating, err := s.validatingController.Get(validatingConfig.Name, metav1.GetOptions{})
-	if apierrors.IsNotFound(err) {
-		_, err = s.validatingController.Create(validatingConfig)
-		if err != nil {
-			return fmt.Errorf("failed to create validating configuration: %w", err)
-		}
-	} else if err != nil {
-		return fmt.Errorf("failed to get validating configuration: %w", err)
-	} else {
-		currValidating.Webhooks = validatingConfig.Webhooks
-		_, err = s.validatingController.Update(currValidating)
-		if err != nil {
-			return fmt.Errorf("failed to update validating configuration: %w", err)
-		}
+	logrus.Infof("listening on :%d serving certs from %s", webhookHTTPSPort, certDir)
+	// Pre-load once so a missing/unreadable cert surfaces at startup rather than per-request.
+	if _, err := tls.LoadX509KeyPair(certPath, keyPath); err != nil {
+		return fmt.Errorf("failed to load serving cert from %s: %w", certDir, err)
 	}
-
-	currMutation, err := s.mutatingController.Get(mutatingConfig.Name, metav1.GetOptions{})
-	if apierrors.IsNotFound(err) {
-		_, err = s.mutatingController.Create(mutatingConfig)
-		if err != nil {
-			return fmt.Errorf("failed to create mutating configuration: %w", err)
-		}
-	} else if err != nil {
-		return fmt.Errorf("failed to get mutating configuration: %w", err)
-	} else {
-		currMutation.Webhooks = mutatingConfig.Webhooks
-		_, err = s.mutatingController.Update(currMutation)
-		if err != nil {
-			return fmt.Errorf("failed to update mutating configuration: %w", err)
-		}
+	if err := server.ListenAndServeTLS("", ""); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("webhook server stopped: %w", err)
 	}
-
 	return nil
+}
+
+// certReloader returns a GetCertificate that re-reads the cert files on every TLS
+// handshake. This is intentionally re-read each time so that needacert can rotate
+// the underlying secret (kubelet refreshes the projected files on its own cycle)
+// without needing a webhook pod restart.
+func certReloader(certPath, keyPath string) func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+	return func(_ *tls.ClientHelloInfo) (*tls.Certificate, error) {
+		cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+		if err != nil {
+			return nil, err
+		}
+		return &cert, nil
+	}
 }
 
 // certAuth returns a middleware for cert-based authentication.
