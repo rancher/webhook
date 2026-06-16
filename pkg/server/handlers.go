75337bca6ab3a7a7508e163f9f130e4d05795718
Tim Hardeck <thardeck@suse.com>
2026-06-16 16:42:38 +0200

    Align fleetworkspace webhook handling with controller flow
    The FleetWorkspace mutating admission handler is removed from the
    webhook server registration, and the associated mutator implementation
    is deleted. This keeps webhook mutation behavior focused on request
    shaping and avoids creating workspace side-effect resources in the
    webhook process.
    
    Webhook configuration reconciliation continues to rebuild the mutating
    webhook list from registered handlers at startup. A regression test is
    added to verify configuration updates replace webhook entries rather
    than accumulating stale entries.

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -18,7 +18,6 @@ import (
 	"github.com/rancher/webhook/pkg/resources/management.cattle.io/v3/clusterproxyconfig"
 	"github.com/rancher/webhook/pkg/resources/management.cattle.io/v3/clusterroletemplatebinding"
 	"github.com/rancher/webhook/pkg/resources/management.cattle.io/v3/feature"
-	"github.com/rancher/webhook/pkg/resources/management.cattle.io/v3/fleetworkspace"
 	"github.com/rancher/webhook/pkg/resources/management.cattle.io/v3/globalrole"
 	"github.com/rancher/webhook/pkg/resources/management.cattle.io/v3/globalrolebinding"
 	"github.com/rancher/webhook/pkg/resources/management.cattle.io/v3/nodedriver"
@@ -116,7 +115,6 @@ func Mutation(clients *clients.Clients) ([]admission.MutatingAdmissionHandler, e
 	mutators := []admission.MutatingAdmissionHandler{
 		provisioningCluster.NewProvisioningClusterMutator(clients.Core.Secret(), clients.Management.PodSecurityAdmissionConfigurationTemplate().Cache()),
 		managementCluster.NewManagementClusterMutator(),
-		fleetworkspace.NewMutator(clients),
 		&machineconfig.Mutator{},
 	}
 
