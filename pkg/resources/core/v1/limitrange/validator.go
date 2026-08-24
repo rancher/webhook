e4980d8ad67611b189816a679c7ff62dd3a34917
Andreas Kupries <andreas.kupries@suse.com>
2026-08-24 16:15:31 +0200

    allow kube system namespace controller to delete all quotas, limit ranges

--- pkg/resources/core/v1/limitrange/validator.go
+++ pkg/resources/core/v1/limitrange/validator.go
@@ -15,6 +15,11 @@ import (
 	"k8s.io/utils/trace"
 )
 
+// kubernetesNamespaceController is the system user who is allowed to all limit
+// ranges, even the rancher-managed one. because it does so only as part of
+// deleting the entire namespace.
+const kubernetesNamespaceController = "system:serviceaccount:kube-system:namespace-controller"
+
 // defaultLimitRangeLabel is the label that Rancher sets on the namespace
 // LimitRange it manages. LimitRanges carrying this label cannot be created,
 // modified, or deleted by regular users. This includes indirect creation by
@@ -104,6 +109,11 @@ func (a *admitter) Admit(request *admission.Request) (*admissionv1.AdmissionResp
 			), nil
 		}
 	case admissionv1.Delete:
+		if request.UserInfo.Username == kubernetesNamespaceController {
+			// The kubernetes controller is allowed to delete the rancher-managed resource.
+			// This happens as part of deleting the containing namespace.
+			return admission.ResponseAllowed(), nil
+		}
 		if hasMarkerLabel(oldRq) {
 			// Reject the user's attempt to delete the
 			// rancher-managed quota resource
